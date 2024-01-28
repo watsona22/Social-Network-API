d849e98a61e19a899765d3a589a3076ee2c5f70d
Lady Godiva <watson.amber.denise@gmail.com>
2024-01-28 12:11:16 -0700

    added user controller and apis

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,4 +2,35 @@ const { User } = require('../models');
 
 module.exports = {
 
-}
+    // get all users
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            console.error({ message: err });
+            return res.status(500).json(err);
+        }
+    },
+    // get a user by id
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId });
+
+            !post
+                ? res.status(404).json({ message: 'No user with that ID' })
+                : res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // create a new user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+};
